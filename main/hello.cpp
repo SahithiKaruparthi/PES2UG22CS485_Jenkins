c75d81a4085c7b093732cf6015b0ca92a2106983
SahithiKaruparthi <sahithikaruparthi@gmail.com>
2025-03-13 16:55:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "I am Sahithi Sri Varshini Karuparthi. My SRN is PES2UG22CS485.\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
